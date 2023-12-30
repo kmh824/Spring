afdea2fc93245eb0e9e9d2806b5d0bac920ce0f0
kmh824 <1rkdalsgur1@naver.com>
2023-12-31 00:49:27 +0900

    commit message

--- src/main/java/hello/hellospring/repository/MemberRepository.java
+++ src/main/java/hello/hellospring/repository/MemberRepository.java
@@ -6,9 +6,15 @@ import java.util.List;
 import java.util.Optional;
 
 public interface MemberRepository {
+    //Member객체의 Id를 setting해서 저장하고 리턴하는 함수.
     Member save(Member member);
-    Optional<Member> findById(Long id); //Optional은 객체를 감싸는 래퍼 클래스로,
-                                        //null을 반환할 수도 있는 메서드를 사용할 때 발생할 수 있는 NullPointerException을 방지하기 위해 사용됩니다.
+
+    //Id가 일치하는 Member객체를 리턴하는 함수.
+    Optional<Member> findById(Long id);
+
+    //Name이 일치하는 Member객체 리턴하는 함수.
     Optional<Member> findByName(String name);
+
+    //존재하는 모든 Member객체 리턴하는 함수.
     List<Member> findAll();
 }
--- src/main/java/hello/hellospring/repository/MemoryMemberRepository.java
+++ src/main/java/hello/hellospring/repository/MemoryMemberRepository.java
@@ -1,14 +1,17 @@
 package hello.hellospring.repository;
 
 import hello.hellospring.domain.Member;
-
 import java.util.*;
 
+//구현체
 public class MemoryMemberRepository implements MemberRepository {
 
+    //회원 <Id, Name>을 담을 수 있는 HashMap store 생성.
     private static Map<Long, Member> store = new HashMap<>();
+    //회원 Id 변수.
     private static long sequence = 0L;
 
+    //Member객체의 Id를 setting해서 저장하고 리턴하는 함수.
     @Override
     public Member save(Member member) {
         member.setId(++sequence);
@@ -16,23 +19,27 @@ public class MemoryMemberRepository implements MemberRepository {
         return member;
     }
 
+    //Id가 일치하는 Member객체를 리턴하는 함수.
     @Override
     public Optional<Member> findById(Long id) {
-        return Optional.ofNullable(store.get(id)); //Optional.ofNullable() 은 값이 Null인 경우에도 오류없이 반환하기 위함.
+        return Optional.ofNullable(store.get(id));
     }
 
+    //Name이 일치하는 Member객체 리턴하는 함수.
     @Override
     public Optional<Member> findByName(String name) {
         return store.values().stream()
                 .filter(member -> member.getName().equals(name))
-                .findAny(); //filter 조건에 일치하는 요소 1개를 Optional로 리턴한다. 조건에 일치하는 요소가 없으면 empty리턴.
+                .findAny();
     }
 
+    //존재하는 모든 Member객체 리턴하는 함수.
     @Override
     public List<Member> findAll() {
         return new ArrayList<>(store.values());
     }
 
+    //store를 clear하는 함수.
     public void clearStore() {
         store.clear();
     }
